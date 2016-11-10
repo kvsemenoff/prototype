cf52e36d13a511609e1be8b78477bb585000c869
Залим Лампежев <sabolch@yandex.ru>
2016-11-10 16:53:35 +0300

    single manufacture

--- wp-content/themes/prototype/functions.php
+++ wp-content/themes/prototype/functions.php
@@ -682,7 +682,7 @@ add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
 
 function special_nav_class ($classes, $item) {
     if (in_array('current-menu-item', $classes) ){
-        $classes[] = 'tech_menu_active ';
+        $classes[] = 'tech_menu_active dg-cost';
     }
     return $classes;
 }
