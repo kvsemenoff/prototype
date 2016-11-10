65ad466b6a5b62bf4ed05777a3b3600082ad7489
nvlight <lightstyle11@mail.ru>
2016-11-10 18:04:41 +0300

    add page tamplate-service.php;

---
+++ wp-content/themes/prototype/template-service.php
@@ -0,0 +1,140 @@
+<?php 
+/*
+Template name: Услуги 
+*/
+?>
+<?php get_header(); ?>     
+<!-- container -->
+<div class="container">
+    <div>
+        <div class="breadcrumbs">
+            <a href="#">Главная</a>\
+            <span>Услуги</span>  
+        </div>
+        <div class="about">
+            <h1>Услуги типографии</h1>
+            <p>На этой странице перечислен полный ассортимент продукции, печать или изготовление которой можно заказать в типографии «ПРОТОТИП».</p>
+        </div>
+        <div class="service_page">
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Дизайн</a>
+                    <div class="ser_menu">
+                        <ul>
+                            <li><a href="#">Брелоки</a></li>
+                            <li><a href="#">Ежедневники</a></li>
+                            <li><a href="#">Флешки</a></li>
+                            <li><a href="#">Ручки</a></li>
+                            <li><a href="#">Зарядные устройства</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser2.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Печатная продукция премиум класса</a>
+                    <div class="ser_menu">
+                        <ul>
+                            <li><a href="#">Брошюры на скобе</a></li>
+                            <li><a href="#">Брошюры на клею</a></li>
+                            <li><a href="#">Брошюры на пружине</a></li>
+                            <li><a href="#">Брошюры на болтах</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser3.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Визитные карточки</a>
+                    <div class="ser_menu">
+                        <ul>
+                            <li><a href="#">Стандартные визитки</a></li>
+                            <li><a href="#">Дизайнерские визитки</a></li>
+                            <li><a href="#">Многослойные визитки</a></li>
+                            <li><a href="#">Эксклюзивные визитки</a></li>
+                            <li><a href="#">Эко-визитки</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser4.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Буклеты и Плакаты</a>
+                    <div class="ser_menu">
+                        <ul>
+                            <li><a href="#">Стандартные каталоги</a></li>
+                            <li><a href="#">Эксклюзивные каталоги</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser5.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Предложение для кафе и ресторанов</a>
+                    <div class="ser_menu">
+                        <ul>
+                            <li><a href="#">Дешевые листовки</a></li>
+                            <li><a href="#">Презентабельные листовки</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser6.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Брендированная упаковка</a>
+                    <div class="ser_menu">
+                        <ul>
+                            <li><a href="#">Стандартные открытки</a></li>
+                            <li><a href="#">Дизайнерские открытки</a></li>
+                            <li><a href="#">Нестандартные открытки</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Наклейки</a>
+                </div>
+            </div>
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Наклейки</a>
+                </div>
+            </div>
+            <div class="service_page_link">
+                <a href="#" class="ser_img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" />
+                </a>
+                <div class="ser_txt">
+                    <a href="#" class="ser_title">Наклейки</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /container -->
+<?php get_footer(); ?>
