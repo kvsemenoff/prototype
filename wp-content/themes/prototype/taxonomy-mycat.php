d4a9f11935fbb9431b2c276feb1057febbe08624
nvlight <lightstyle11@mail.ru>
2016-11-12 16:48:22 +0300

    sql

--- wp-content/themes/prototype/taxonomy-mycat.php
+++ wp-content/themes/prototype/taxonomy-mycat.php
@@ -1,54 +1,95 @@
-<?php get_header(); ?>
-<div class="service_page_link">
-  	<?php $hiterms = get_terms("mycat", array("orderby" => "slug", "parent" => 0)); ?>
-    <?php foreach($hiterms as $key => $hiterm) : ?>
-    		<?php 
+<?php get_header(); ?>  
+tttttttttttttttttttttt
+<?php
+$term = get_queried_object();
+$children = get_terms( $term->taxonomy, array(
+'parent'    => $term->term_id,
+'hide_empty' => false
+) );
 
-    			//$image = get_image('картинка_для_категории_услуги', $hiterm);
-    			//$arr = get_field('картинка_для_категории_услуги');  print_r($arr); 
+if( $children ){
+   echo "ddd";
 
-  			// vars
+} else { 
+        // здесь код следующего уровня
+    lavel2();
+   
+}
+?>
 
 
 
-    		?>
-	    	<a href="#" class="ser_img">
-	      	<?php 
 
-					$queried_object = get_queried_object(); 
+<?php get_footer(); ?>
 
-					$taxonomy = $queried_object->taxonomy;
-					$term_id = $queried_object->term_id;  
-					  			$image = get_field('mycatimg', $taxonomy . '_' . $term_id);
-					echo '<img src="'.$image['sizes']['thumbnail'].'" />';
+<?php
 
-	      	?>
-	      
-
-	      </a>
-	      <div class="ser_txt">
-	          <a href="<?php echo $hiterm->slug; ?>" class="ser_title"><?php echo $hiterm->name; ?></a>
-	          <div class="ser_menu">
-	          	<ul>
- 						
-		            <?php $loterms = get_terms("mycat", array("orderby" => "slug", "parent" => $hiterm->term_id)); ?>
-		            <?php if($loterms) : ?>
-                
-                    <?php foreach($loterms as $key => $loterm) : ?>
-                        <li><a href="<?php echo $loterm->slug; ?>"><?php echo $loterm->name; ?></a></li>
-                    <?php endforeach; ?>
+function lavel2(){
+    ?>
+           
+    <div class="container">
+        <div>
+            <div class="breadcrumbs">
+                    <?php if( function_exists('kama_breadcrumbs') ) kama_breadcrumbs(' \ '); ?>
+                </div>
+            <div class="service_i_top clearfix">
+                <h1><?php the_title(); ?></h1>
+                <div class="service_i_txt">
+                    <p>Выберите визитку, которую вы хотели бы заказать из предложенных ниже. Если не нашли подходящую – позвоните нам или отправьте сообщение через форму, расположенную внизу страницы.</p>
+                </div>
+            </div>
+            <div class="service_inside clearfix">
+                <?php 
+                $term = get_queried_object();
+                 wp_reset_query();
+                 $args = array(
+                    'post_type' => 'de_services',
+                    'mycat'    => $term->name
+                );
+                $query = new WP_Query( $args );
+                if ( $query->have_posts() ) {
                
-           		 	<?php endif; ?>
-	              	
-	              </ul>
-	          </div>
-	      </div>
+                    while ( $query->have_posts() ) {
+                        $query->the_post();
+                        ?>
+                        <div class="ser_i_link">            
+                            <a href="<?php the_permalink(); ?>" class="ser_l_img"><img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" /></a>
+                            <div class="ser_i_txt">
+                                <a href="<?php the_permalink(); ?>" class="ser_i_title"><?php the_title(); ?></a>
+                                <ul>    
+                                    <li>Обычные визитки на качественной плотной бумаге</li>
+                                    <li>Самый недорогой вариант из всех возможных</li>
+                                    <li>Оптимально подходят для массового распространения</li>
+                                </ul>
+                            </div>
+                            <a href="<?php the_permalink(); ?>" class="bordered_btn">ПОДРОБНЕЕ</a>
+                        </div>
+                <?php        
+                    }
+                } else {
+                    // Постов не найдено
+                }
+                /* Возвращаем оригинальные данные поста. Сбрасываем $post. */
+                wp_reset_query();
+                ?>
+                
+            </div>
+            <div class="service_inside_btm">
+                <h2>Вот некоторые визитки, которые мы печатаем:</h2>
+                <ul>
+                    <li>стандартные визитки – для них применяется белая плотная бумага. Подходят для тех, кто стеснен в бюджете или нуждается в визитках без изысков для массового распространения</li>
+                    <li>многослойные визитки – двух- или трехслойные с цветной вставкой внутри. внимание.</li>
+                    <li>дизайнерские визитки – печатаются на дизайнерских бумагах и картонах, в том числе белой краской. Отделка - на вкус заказчика: слепое тиснение, тиснение фольгой, вырубка и проч.</li>
+                    <li>эксклюзивные визитки - космос! Описать словами трудно. Приезжайте в гости, и мы покажем образцы.</li>
+                    <li>Эко-визитки - делаются только из переработанной бумаги. Не популярный сегодня вариант, но интерес к ним растет.</li>
+                </ul>
+                <p>Отдельно отметим, что в типографии ПРОТОТИП заказывают визитки по индивидуальному проекту. Вы рассказываете, что хотите, а мы придумываем, как это сделать!</p>
+            </div>
+        </div>
+    </div>
+    <?php
 
-       
-    <?php endforeach; ?>
- </div>
+}
 
- <?php get_footer(); ?>
-
-
+?>
 
--- wp-content/themes/prototype/template-service.php
+++ wp-content/themes/prototype/template-service.php
@@ -3,206 +3,59 @@
 Template name: Услуги
 */
 ?>
-<?php get_header(); ?>     
+<?php get_header(); ?>  
 
-<?php
-function d($val, $caption="", $die=0){
-    echo "<br><span style='color: red; '>DEBUG {$caption} </span><pre>"; 
-    print_r($val); 
-    echo "</pre>"; 
-
-    if ($die) { die(); }
-}
-?>
-
-<!-- container -->
 <div class="container">
     <div>
-        <div class="breadcrumbs">
-            <a href="#">Главная</a>\
-            <span>Услуги</span>
+         <div class="breadcrumbs">
+            <?php if( function_exists('kama_breadcrumbs') ) kama_breadcrumbs(' \ '); ?>
         </div>
         <div class="about">
             <h1>Услуги типографии</h1>
             <p>На этой странице перечислен полный ассортимент продукции, печать или изготовление которой можно заказать в типографии «ПРОТОТИП».</p>
         </div>
-        <!-- start of Nav_services -->
+        <div class="service_page">
         <?php 
-            global $wpdb; 
-            $query = "SELECT COUNT(*) FROM posts";
-            //$wpdb->query($query);
-            $rs = $wpdb->get_results($query);
-            d($rs, "query 1", 1); 
-
-            $query = new WP_Query( array( 'post_type' => 'de_services', 'publish' => true) ); 
-            $query = $query['posts']; 
-            d($query,"de_services",1); 
-
             $hiterms = get_terms("mycat", array("orderby" => "slug", "parent" => 0));
-            //d($hiterms,"parent"); 
             foreach($hiterms as $key => $hiterm){
-                d($hiterm,"hiterm_inner"); 
-                //$image = get_image('картинка_для_категории_услуги', $hiterm); 
-                //$arr = get_field('картинка_для_категории_услуги');  print_r($arr); 
-                $queried_object = get_queried_object(); 
-                d($queried_object,"inner object. id = "); 
-                $taxonomy = $queried_object->taxonomy; 
-                $term_id = $queried_object->term_id; 
-                $image = get_field('mycatimg', $taxonomy . '_' . $term_id); 
-                d($image,"img"); 
-                // $image = get_field('name'); 
-                echo 'img = <img src="'.$image['sizes']['thumbnail'].'" /> :_ end<br/>'; 
-                echo $hiterm->name; 
-                echo $hiterm->slug;                 
-
-                // 
-                // $query = new WP_Query( array( 'post_type' => 'de_services', 'publish' => true) ); 
-                // d($query,"de_services",1); 
-                //  if ($query->have_posts())
-                //     while ( $query->have_posts() ) { 
-                //       $query->the_post(); 
-
-
-                //       echo get_the_content(); 
-                //       echo get_field('uk-fio');
-                //   }
-                wp_reset_query();
-
-                $loterms = get_terms("mycat", array("orderby" => "slug", "parent" => $hiterm->term_id)); 
-                if($loterms){
-                    foreach($loterms as $key => $loterm){
-                    }
-                }
+                $image = get_field('mycatimg', 'mycat' . '_' . $hiterm->term_id); 
+                ?>
+                <div class="service_page_link">
+                    <a href="#" class="ser_img">
+                        <img src="<?php echo $image['sizes']['thumbnail'] ?>" alt="">
+                    </a>
+                    <div class="ser_txt">
+                        <a href="<?php  echo $hiterm->slug;  ?>" class="ser_title"><?php echo $hiterm->name; ?></a>
+                        <div class="ser_menu">
+                            <?php 
+                             wp_reset_query();
+                             $args = array(
+                                'post_type' => 'de_services',
+                                'mycat'    => $hiterm->name
+                            );
+                            $query = new WP_Query( $args );
+                            if ( $query->have_posts() ) {
+                           
+                                while ( $query->have_posts() ) {
+                                    $query->the_post();
+                                    ?><li><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></li><?php
+                                   
+                                }
+                            } else {
+                                // Постов не найдено
+                            }
+                            /* Возвращаем оригинальные данные поста. Сбрасываем $post. */
+                            wp_reset_query();
+                            ?>
+                        </div>
+                    </div>
+                </div>
+                <?php
             }
             ?>
-
-        <!-- end of Nav_services -->
-        <hr>
-        <hr>
-        <hr>
-
-        <div class="service_page">
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Дизайн</a>
-                    <div class="ser_menu">
-                        <ul>
-                            <li><a href="#">Брелоки</a></li>
-                            <li><a href="#">Ежедневники</a></li>
-                            <li><a href="#">Флешки</a></li>
-                            <li><a href="#">Ручки</a></li>
-                            <li><a href="#">Зарядные устройства</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser2.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Печатная продукция премиум класса</a>
-                    <div class="ser_menu">
-                        <ul>
-                            <li><a href="#">Брошюры на скобе</a></li>
-                            <li><a href="#">Брошюры на клею</a></li>
-                            <li><a href="#">Брошюры на пружине</a></li>
-                            <li><a href="#">Брошюры на болтах</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser3.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Визитные карточки</a>
-                    <div class="ser_menu">
-                        <ul>
-                            <li><a href="#">Стандартные визитки</a></li>
-                            <li><a href="#">Дизайнерские визитки</a></li>
-                            <li><a href="#">Многослойные визитки</a></li>
-                            <li><a href="#">Эксклюзивные визитки</a></li>
-                            <li><a href="#">Эко-визитки</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser4.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Буклеты и Плакаты</a>
-                    <div class="ser_menu">
-                        <ul>
-                            <li><a href="#">Стандартные каталоги</a></li>
-                            <li><a href="#">Эксклюзивные каталоги</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser5.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Предложение для кафе и ресторанов</a>
-                    <div class="ser_menu">
-                        <ul>
-                            <li><a href="#">Дешевые листовки</a></li>
-                            <li><a href="#">Презентабельные листовки</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser6.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Брендированная упаковка</a>
-                    <div class="ser_menu">
-                        <ul>
-                            <li><a href="#">Стандартные открытки</a></li>
-                            <li><a href="#">Дизайнерские открытки</a></li>
-                            <li><a href="#">Нестандартные открытки</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Наклейки</a>
-                </div>
-            </div>
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Наклейки</a>
-                </div>
-            </div>
-            <div class="service_page_link">
-                <a href="#" class="ser_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/ser1.jpg" alt="" />
-                </a>
-                <div class="ser_txt">
-                    <a href="#" class="ser_title">Наклейки</a>
-                </div>
-            </div>
-
-
         </div>
+        <!-- end of Nav_services -->
     </div>
 </div>
-<!-- /container -->
+
 <?php get_footer(); ?>
